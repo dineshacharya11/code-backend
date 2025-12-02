ad5b61524c53026aeedf68ea89f2dd17d5bf666a
dineshacharya11 <adinesh915@gmail.com>
2025-12-02 14:13:11 +0530

    add register contriller and bug fix

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -6,6 +6,10 @@ class ApiResponse{
         this.success = statusCode < 400
     }
 }
+
+
+
+export { ApiResponse }
 /*  server status code (HTTP response status code)
 1.Informational response (100-199)
 2.Successful response (200-299)
